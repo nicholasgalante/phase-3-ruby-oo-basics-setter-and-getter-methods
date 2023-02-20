bacead48024595e63b69ba519bf3d352564cb6f7
Nicholas Galante <nicholasgalante94@gmail.com>
2023-02-20 16:41:25 -0500

    Define getter and setter methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+   attr_accessor :job
 
+   def name=(name)
+      @name = name
+   end
+
+   def name
+      @name
+   end
+
+   def job=(job)
+      @job = job
+   end
+
+   # def job
+   #    @job
+   # end
 end
