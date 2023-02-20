436136a28305febe39b3f0c7e8b3d3c81687af5d
Nicholas Galante <nicholasgalante94@gmail.com>
2023-02-20 16:41:07 -0500

    commit tree 268bb72fdf808fa7a8f646e1bfeaa613731f2679

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,33 @@
 class Dog
+   def name=(name)
+      @name = name
+   end
 
+   def name 
+      @name
+   end
+
+   def breed=(breed)
+      @breed = breed
+   end
+
+   def breed
+      @breed
+   end
 end
+
+
+
+# class Person
+
+#    # setter method
+#    def name=(name)
+#      @name = name
+#    end
+ 
+#    # getter method
+#    def name
+#      @name
+#    end
+ 
+#  end
